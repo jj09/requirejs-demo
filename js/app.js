a9f4ceba4ceffd48526f4cf422c6002c25948f23
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2016-08-08 22:42:01 -0700

    add RequireJS

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-$("#getPeople").click(function () {
-    var people = ['Jakub', 'Bill', 'Steve'];
+require(['jquery', 'underscore', 'videojs'], function ($, _, videojs) {
+    $("#getPeople").click(function () {
+        var people = ['Jakub', 'Bill', 'Steve'];
 
-    $(".people").append("<h2>" + _.size(people) + " people in database</h2>");
+        $(".people").append("<h2>" + _.size(people) + " people in database</h2>");
 
-    _.each(people, function (person) {
-        $(".people").append("<p>" + person + " <button class='video'>video</button></p>");
-    });
+        _.each(people, function (person) {
+            $(".people").append("<p>" + person + " <button class='video'>video</button></p>");
+        });
 
-    $(".video").click(function () {
-        $(".player").html(
-            "<video id='video' class='video-js' controls preload='none' width='480' height='360' data-setup='{}'>" +
-                "<source src='video.mp4' type='video/mp4'>" +
-            "</video>"
-            );
-        videojs('video');
+        $(".video").click(function () {
+            $(".player").html(
+                "<video id='video' class='video-js' controls preload='none' width='480' height='360' data-setup='{}'>" +
+                    "<source src='video.mp4' type='video/mp4'>" +
+                "</video>"
+                );
+            videojs('video');
+        });
     });
 });
