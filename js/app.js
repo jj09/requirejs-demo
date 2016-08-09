5d684032876090a34020d9851f1efafa7cda1c2f
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2016-08-08 22:52:59 -0700

    lazy load js and css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-require(['jquery', 'underscore', 'videojs'], function ($, _, videojs) {
+require(['jquery'], function ($) {
     $("#getPeople").click(function () {
         var people = ['Jakub', 'Bill', 'Steve'];
 
-        $(".people").append("<h2>" + _.size(people) + " people in database</h2>");
+        require(['underscore'], function (_) {
+            $(".people").append("<h2>" + _.size(people) + " people in database</h2>");
 
-        _.each(people, function (person) {
-            $(".people").append("<p>" + person + " <button class='video'>video</button></p>");
-        });
+            _.each(people, function (person) {
+                $(".people").append("<p>" + person + " <button class='video'>video</button></p>");
+            });
 
-        $(".video").click(function () {
-            $(".player").html(
-                "<video id='video' class='video-js' controls preload='none' width='480' height='360' data-setup='{}'>" +
-                    "<source src='video.mp4' type='video/mp4'>" +
-                "</video>"
-                );
-            videojs('video');
-        });
+            $(".video").click(function () {
+                require(['videojs'], function (videojs) {
+                    $("head").append("<link href=\"dist/css/video-js.css\" rel=\"stylesheet\">");
+                    $(".player").html(
+                        "<video id='video' class='video-js' controls preload='none' width='480' height='360' data-setup='{}'>" +
+                            "<source src='video.mp4' type='video/mp4'>" +
+                        "</video>"
+                        );
+                    videojs('video');
+                });
+            });
+        });        
     });
 });
