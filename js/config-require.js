f02eface669ae134025e31c8b4e6c60e06607a30
Jakub Jedryszek <jakub.jedryszek@gmail.com>
2016-08-08 23:00:49 -0700

    add CDN

--- js/config-require.js
+++ js/config-require.js
@@ -1,9 +1,18 @@
 requirejs.config({
     baseUrl: 'dist/js',
     paths: {
-        jquery: 'jquery-3.1.0',
-        underscore: 'underscore',
-        videojs: 'video'
+        jquery: [
+            'https://code.jquery.com/jquery-3.1.0.min',
+            'jquery-3.1.0'
+            ],
+        underscore: [
+            'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min',
+            'underscore'
+            ],
+        videojs: [
+            'http://vjs.zencdn.net/5.10.8/video.min',
+            'video'
+            ]
     }
 });
 
